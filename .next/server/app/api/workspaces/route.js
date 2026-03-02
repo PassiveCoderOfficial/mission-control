0c60180ad6fa40c0d46e7131fbc1d8650460bf1d
Ubuntu <ubuntu@ip-172-31-21-91.ap-southeast-1.compute.internal>
2026-03-02 20:09:49 +0000

    Remove node_modules and build artifacts from Git, update .gitignore

--- .next/server/app/api/workspaces/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/workspaces/route.js")
-R.c("server/chunks/[externals]_next_dist_a6d89067._.js")
-R.c("server/chunks/[root-of-the-server]__31c8fa78._.js")
-R.c("server/chunks/_next-internal_server_app_api_workspaces_route_actions_0cd846ef.js")
-R.m(41552)
-module.exports=R.m(41552).exports
--- .next/server/chunks/ssr/_next-internal_server_app_workspaces_page_actions_fa7d719f.js
+++
@@ -1,3 +0,0 @@
-module.exports=[35925,(a,b,c)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_workspaces_page_actions_fa7d719f.js.map
